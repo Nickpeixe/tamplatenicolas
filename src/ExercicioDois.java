5ce35558bb2ea6f10138fff26507068592c3febf
Nickpeixe <nick.h.texeira11@gmail.com>
2022-08-29 16:02:35 -0300

    Modificação Atividade 1 e 2

--- src/ExercicioDois.java
+++ src/ExercicioDois.java
@@ -31,35 +31,89 @@ public class ExercicioDois {
         switch (menu) {
             // 1) Crie um programa que receba três notas calcule a média e diga se foi aprovado (`(nota1 + nota2 + nota3 / 3)`) e (`média maior que 7 para ser aprovado`).
             case 1:
-                // calculaMedia(double notaUm, double notaDois, double notaTres);
+                System.out.println("Programa que calcula a média de três valores.");
+                System.out.println("Digite o primeiro valor: ");
+                double notaUm = scanner.nextDouble();
+                System.out.println("Digite o segundo valor: ");
+                double notaDois = scanner.nextDouble();
+                System.out.println("Digite o terceiro valor: ");
+                double notaTres = scanner.nextDouble();
+                System.out.println("A média das três notas é: " + calculaMedia(notaUm, notaDois, notaTres));
+                if(calculaMedia(notaUm, notaDois, notaTres) >= 7){
+                    System.out.println("Você foi aprovado!");
+                } else {
+                    System.out.println("Você foi reprovado!");
+                }
                 break;
             // 2) Crie um programa que receba um número e indique o mês que representa.
             case 2:
-                // qualMes(int mes);
+                System.out.println("Programa que recebe um número e retorna o mês representante. ");
+                System.out.println("Digite um valor de 1 à 12: ");
+                int mes = scanner.nextInt();
+                System.out.println("O mês representado pelo número é: " + qualMes(mes));
                 break;
             // 3) Crie um programa que simule um jogo da forca, com a entrada da palavra oculta, a quantidade de tentativas para acerto e depois solicite a entrada da palavra. A cada erro deve imprimir a quantidade de tentativas restantes e o 'membro' que foi marcado. Ao final deve imprimir se acertou ou não a palavra e se foi 'enforcado'.
             case 3:
+                System.out.println("Programa que simula o jogo de forca. ");
+                System.out.println("Digite a palavra: ");
+                String palavr4 = scanner.next();
+                System.out.println("Digite a quantidade de tentativas: ");
+                int tentativa = scanner.nextInt();
+                System.out.println("Tente adivinhar qual é a palavra");
+                for(int i = tentativa; i > 0; i--)
+                {
+                    System.out.println("Você tem " + i + " tentativas");
+                    String scan = scanner.next();
+                    if(scan.equals(palavr4)) {
+                        System.out.println("Acertou");
+                        break;
+                    } else {
+                        System.out.println("Errou");
+                    }      
+                }
                 // forca(String palavra, int tentativa, Scanner scanner);
                 break;
             // 4) Crie um programa que imprima a tabuada de 0 a 10.
             case 4:
-                // tabuada(int numero);
+                System.out.println("Programa que imprime a tabuada de 0 à 10. ");
+                System.out.println("Digite um valor: ");
+                int numero = scanner.nextInt();
+                System.out.println("Resultado: " + Arrays.toString(tabuada(numero)));
                 break;
             // 5) Crie um programa que receba uma String e imprima linha a linha suas letras.
             case 5:
-                // letraALetra(String palavra);
+                System.out.println("Programa que imprime linha a linha as letras de uma string. ");
+                System.out.println("Digite a palavra: ");
+                String palavra = scanner.next();
+                for(char letra : letraALetra(palavra)){
+                    System.out.println(letra);
+                }
                 break;
             // 6) Crie um programa que de a soma de todos os números ímpares e múltiplos de 7 entre 1 e 500.
             case 6:
+                System.out.println("Programa que dá a soma de todos os números ímpares e múltiplos de 7 entre 1 e 500. ");
+                System.out.println("O resultado é: " + numerosImpares());
                 // numerosImpares();
                 break;
             // 7) Crie um programa que tenha como entrada as notas de um aluno. Somente se encerrará quando for inserida uma nota negativa. Ao final deverá informar a média das notas.
             case 7:
-                // calculaMedia(double[] notas);
+                System.out.println("Programa que calcula as médias das notas de um aluno.");
+                double[] notas = new double[500];
+                for(int i = 1; i < notas.length; i++) {
+                    System.out.println("Digite a " + i + "º nota: ");
+                    notas[i - 1] = scanner.nextDouble();
+                    if(notas[i -1] < 0) {
+                        System.out.println("O resultado da média das notas é: " + calculaMedia(notas));
+                        break;
+                    }
+                }
                 break;
             // 8) Crie um programa que receba um valor e imprima o fatorial desse número. Ex.: 3! = 3 x 2 x 1 = 6.
             case 8:
-                // fatorial(int numero);
+                System.out.println("Programa que imprime o fatorial de um número. ");
+                System.out.println("Digite um valor: ");
+                int numer0 = scanner.nextInt();
+                System.out.println("O fatorial é: " + fatorial(numer0));
                 break;
             // 9) Crie um programa que receba a altera e o peso e calcule o IMC:
             // 
@@ -72,11 +126,35 @@ public class ExercicioDois {
             // Entre 35,0 e 39,9 | Obesidade Grau II
             // Acima de 40 | Obesidade Grau III (mórbida)
             case 9:
-                // imc(double altura, double peso);
+                System.out.println("Programa que calcula IMC. ");
+                System.out.println("Digite sua altura em métros: ");
+                double altura = scanner.nextDouble();
+                System.out.println("Digite  seu peso em kg: ");
+                double peso = scanner.nextDouble();
+                if(imc(altura, peso) < 18.5) {
+                    System.out.println("Você está abaixo do peso. ");
+                } else if((imc(altura, peso) > 18.6) && (imc(altura, peso) < 24.9)) {
+                    System.out.println("Você está no peso ideal. ");
+                } else if((imc(altura, peso) > 25) && (imc(altura, peso) < 29.9)) {
+                    System.out.println("Você está levemente acima do peso. ");
+                } else if((imc(altura, peso) > 30) && (imc(altura, peso) < 34.9)) {
+                    System.out.println("Você está com obesidade grau I. ");
+                } else if((imc(altura, peso) > 35) && (imc(altura, peso) < 39.9)) {
+                    System.out.println("Você está com obesidade grau II. ");
+                } else {
+                    System.out.println("Você está com obesidade grau III. ");
+                }
                 break;
             // 10) Crie um programa que receba dois valores e solicite a operação a ser realizada (+ - * /). Ao final imprima o resultado.
             case 10:
-                // operador(int valorUm, int valorDois, char operacao);
+                System.out.println("Programa que opera com varias operações com dois valores. ");
+                System.out.println("Digite um valor: ");
+                int valorUm = scanner.nextInt();
+                System.out.println("Digite a operação (+, -, *, /): ");
+                char operacao = scanner.next().charAt(0);
+                System.out.println("Digite um outro valor: ");
+                int valorDois = scanner.nextInt();
+                System.out.println("Resultado: " + operador(valorUm, valorDois, operacao));
                 break;
             // DESAFIO: Crie um programa que receba um valor em reais e determine qual o mínimo de notas necessárias para entregá-lo. <br> Notas: R$ 200, R$ 100, R$ 50, R$ 10, R$ 5, R$ 1 <br> Exemplo: R$ 15 -> 1 nota de R$ 10 e 1 nota de R$ 5
             case 11:
@@ -88,39 +166,131 @@ public class ExercicioDois {
     }
 
     public static double calculaMedia(double notaUm, double notaDois, double notaTres) {
-        return 0;
+        double media = (notaUm+notaDois+notaTres)/3;
+        return media;
     }
 
     public static String qualMes(int mes) {
-        return "";
+        switch (mes) {
+            case 1:{
+                return "Janeiro";
+            }
+            case 2:{
+                return "Fevereiro";
+            }
+            case 3:{
+                return "Março";
+            }
+            case 4:{
+                return "Abril";
+            }
+            case 5:{
+                return "Maio";
+            }
+            case 6:{
+                return "Junho";
+            }
+            case 7:{
+                return "Julho";
+            }
+            case 8:{
+                return "Agosto";
+            }
+            case 9:{
+                return "Setembro";
+            }
+            case 10:{
+                return "Outubro";
+            }
+            case 11:{
+                return "Novembro";
+            }
+            case 12:{
+                return "Dezembro";
+            }
+            default:{
+                return "Valor inválido!";
+            }
+        }
     }
 
     public static int[] tabuada(int numero) {
-        return new int[10];
+        int[] vet = new int[10];
+        for(int i = 1; i <= 10; i++){
+            vet[i - 1] = numero * i;
+        }
+        return vet;
     }
 
     public static char[] letraALetra(String palavra) {
-        return new char[1];
+        char[] word = new char[palavra.length()];
+        for(int i = 0; i < word.length; i++){
+            word[i] = palavra.charAt(i);
+        }
+        return word;
     }
 
     public static int numerosImpares() {
-        return 0;
+        int[] num = new int[500];
+        int soma = 0;
+        for(int i = 0; i< 500; i++){
+            num[i] = i;
+        }
+        for(int i = 0; i < 500; i++){
+            if((num[i] % 2 == 1) && (num[i] % 7 == 0)){
+                soma = soma + num[i];
+            }
+        }
+        return soma;
     }
 
     public static double calculaMedia(double[] notas) {
-        return 0;
+        double cont = 0;
+        int j = 0;
+        for(int i = 0; i < notas.length; i++) {
+            if(notas[i] > 0) {
+                cont = cont + notas[i];
+                j++;
+            }   
+        }
+        cont = cont/j;
+        return cont;
     }
 
-    public static int fatorial(int numero) {
+    public static int fatorial(int numer0) {
         return 0;
     }
 
     public static double imc(double altura, double peso) {
-        return 0;
+        double calcimc = peso / (altura*altura);
+        return calcimc;
     }
 
     public static double operador(int valorUm, int valorDois, char operacao) {
-        return 0;
+        double op = 0;
+        switch (operacao) {
+            case '+':{
+                op = valorUm + valorDois;
+                break;
+            }
+            case '-':{
+                op = valorUm - valorDois;
+                break;
+            }
+            case '/':{
+                op = valorUm / valorDois;
+                break;
+            }
+            case '*':{
+                op = valorUm * valorDois;
+                break;
+            }
+            default: {
+                System.out.println("Operador inválido. ");
+                break;
+            }
+        }
+        return op;
     }
 
     public static int[] notasNecessarias(int valor) {
--- src/ExercicioUm.java
+++ src/ExercicioUm.java
@@ -32,26 +32,52 @@ public class ExercicioUm {
             // 1) Crie um programa que calcule a média (`(nota1 + nota2 + nota3 / 3)`)
             case 1:
                 // media(double notaUm, double notaDois, double notaTres)
+                System.out.println("Calculando média");
+                double res1 = media(8, 7, 9);
+                System.out.println(" O valor da média é " +res1);
+               
                 break;
             // 2) Crie um programa que calcule a área (`lado1 * lado2`)
             case 2:
                 // area(double ladoUm, double ladoDois)
+                System.out.println("Calculando a área");
+                double res2 = area(8,8);
+                System.out.println("O valor da área é "+res2);
                 break;
             // 3) Crie um programa que calcule o volume (`largura * altura * profundidade`)
             case 3:
                 // volume(double largura, double altura, double profundidade)
+                System.out.println("Calculando o volume");
+                double res3 = volume(5,5,5);
+                System.out.println("O valor do volume é "+res3);
                 break;
             // 4) Crie um programa que avalie se um valor é maior do que o dobro de outro valor
             case 4:
                 // valorMaiorQueDobro(double base, double verificar)
+                System.out.println("Verificar se um valor é maior que o dobro de outro valor");
+                boolean res4 = valorMaiorQueDobro(2,8);
+                if(res4==true){
+                    System.out.println("O valor é maior que o dobro do outro valor");
+                } else {
+                    System.out.println("O valor é menor que o dobro do outro valor");
+                }
                 break;
             // 5) Crie um programa que calcule _bhaskara_
             case 5:
                 // bhaskara(int a, int b, int c);
+                System.out.println("Calculando bhaskara");
+                double[] res5 = bhaskara(2,6,-5);
+                System.out.println("O valor de x' é " 
+                +res5[0] 
+                + " e x'' é " 
+                + res5[1]);
                 break;
             // 6) Crie um programa que calcule a velocidade média de uma viagem (`distancia (km) / tempo (h)`)
             case 6:
                 // mediaViagem(double distancia, double tempo)
+                System.out.println("Calculando velocidade média");
+                double res6 = mediaViagem(5,5);
+                System.out.println("O valor da velocidade média é " + res6);
                 break;
             // 7) Crie um programa que calcule o percentual de imposto pago (`faturamento \ qtd imposto`)
             case 7:
@@ -60,6 +86,13 @@ public class ExercicioUm {
             // 8) Crie um programa que teste se um valor é par ou ímpar
             case 8:
                 // parOuImpar(int valor)
+                System.out.println("Verificando se o valor é par ou impar");
+                boolean res7 = parOuImpar(31);
+                if(res7 == true) {
+                    System.out.println("O valor é par");
+                } else {
+                    System.out.println("O valor é impar");
+                }
                 break;
             // 9) Crie um programa que compare dois valores Strings
             case 9:
@@ -91,35 +124,60 @@ public class ExercicioUm {
     }
 
     public static double media(double notaUm, double notaDois, double notaTres) {
-        return 0;
+
+        double median = (notaUm + notaDois + notaTres)/3;
+        return median;
     }
 
     public static double area(double ladoUm, double ladoDois) {
-        return 0;
+
+        double areal = ladoUm * ladoDois;
+        return areal;
     }
 
     public static double volume(double largura, double altura, double profundidade) {
-        return 0;
+
+        
+        double vol = largura * altura * profundidade;
+        return vol;
     }
 
     public static boolean valorMaiorQueDobro(double base, double verificar) {
-        return false;
+
+        if((base*2) > verificar){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public static double[] bhaskara(int a, int b, int c) {
-        return new double[2];
+        double delta = (b*b)-(4*a*c);
+        double bhaskarar[] = new double []{0,1};
+        bhaskarar[0] = ((-b)+Math.sqrt(delta))/2*a;
+        bhaskarar[1] = ((-b)-Math.sqrt(delta))/2*a;
+        
+        return bhaskarar;
     }
 
     public static double mediaViagem(double distancia, double tempo) {
-        return 0;
+        double mediaV= distancia * tempo;
+        return mediaV;
     }
 
     public static double percentualImposto(double faturamento, double qtdImposto) {
+        // 7) Crie um programa que calcule o percentual de imposto pago (`faturamento \ qtd imposto`)
+
         return 0;
     }
 
     public static boolean parOuImpar(int valor) {
-        return false;
+        // 8) Crie um programa que teste se um valor é par ou ímpar
+        if(valor%2==0) {
+            return true;
+        } else {
+            return false;
+        }  
     }
 
     public static boolean compareString(String stringUm, String stringDois) {
@@ -131,7 +189,26 @@ public class ExercicioUm {
     }
 
     public static double percentualImpostoSalario(double salario) {
-        return -1;
+        // 11) Crie um programa que receba o salário e diga o valor de imposto de renda conforme abaixo:
+            // Base                     | Alíquota
+            // ---------                | ------
+            // Até 1.903,98             | Isento
+            // De 1.903,99 até 2.826,65 | 7,5%
+            // De 2.826,66 até 3.751,05 | 15%
+            // De 3.751,06 até 4.664,68 | 22,5%
+            // Acima de 4.664,68        | 27,5%
+        if(salario<=1903.98){
+            return 1;
+        } else if (salario>1903.98 && salario<=2826.65) {
+            return 2;
+        } else if (salario>2826.65 && salario<=3751.05) {
+            return 3;
+        } else if (salario>3751.05 && salario<=4664.68) {
+            return 4;
+        } else if (salario>4664.68){
+            return 5;
+        } 
+        return 0;
     }
 
     public static boolean testePrimo(int valor) {
