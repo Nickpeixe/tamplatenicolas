4bcd00cc900e1d2a7bbedf1ee0df568833ca71c7
Nicolas Henrique <nick.h.texeira11@gmail.com>
2022-09-21 16:42:11 -0300

    exercicio 5

--- src/ExercicioCinco.java
+++ src/ExercicioCinco.java
@@ -25,7 +25,7 @@ public class ExercicioCinco {
         switch (menu) {
             // 1) Crie uma classe que represente uma Pessoa, devendo ter as propriedades Nome, Peso, Altura e Data de Nascimento e o método IMC, que retornará o valor do IMC.
             case 1:
-                // criarPessoa();
+                criarPessoa();
                 break;
             /* 2) Crie um programa que receba a pessoa criada e verifique qual o IMC.
 
@@ -37,9 +37,9 @@ public class ExercicioCinco {
             Entre 30,0 e 34,9 | Obesidade Grau I
             Entre 35,0 e 39,9 | Obesidade Grau II
             Acima de 40       | Obesidade Grau III (mórbida)
-            */                
+            */         
             case 2:
-                // verificarIMC();
+                verificarIMC();
                 break;
             // 3) Crie uma classe que represente uma Conta Corrente, devendo ter as propriedades Número da Conta, Agência, Saldo, Nome do Titular e os métodos Depósito, Saque e Saldo Total.
             case 3:
@@ -55,7 +55,7 @@ public class ExercicioCinco {
                 break;
             // 6) Crie uma classe para Cifra de César, devendo ter um método criptografia e receba como valor inicial uma mensagem e o valor constante. Cifra de César é uma das criptografias mais antigas conhecidas, onde a chave está no valor constante, para cada letra do alfabeto atribuí-se um número (exemplo: A=1;B=2;C=3,etc), então para cada letra da mensagem, soma-se o valor correspondente a esta letra ao valor constante, gerando uma nova letra para formar a nova mensagem, ou seja, a mensagem "Ataque a roma" com um valor constante de 3 seria "dwdtxh d urpd". Dica, utilize arrays para isso.
             case 6:
-                // cifraCesar();
+                cifraCesar();
                 break;
             default:
                 System.out.println("Operação inválida");
@@ -65,11 +65,26 @@ public class ExercicioCinco {
     }
 
     public static void criarPessoa() {
-        
+        Pessoa pessoa = new Pessoa("Jackson", 79, 1.70, "31/03/1990");
+        System.out.println(
+            "Nome: " + pessoa.nome + "\n"
+            + "Peso: " + pessoa.peso + "\n"
+            + "Altura: " + pessoa.altura + "\n"
+            + "Data de Nascimento: " + pessoa.dataNascimento + "\n"
+            + "IMC: " + pessoa.imc()
+        );
     }
 
     public static void verificarIMC() {
-        
+        Pessoa pessoa = new Pessoa("Jackson", 79, 1.70, "31/03/1990");
+        System.out.println(
+            "Nome: " + pessoa.nome + "\n"
+            + "Peso: " + pessoa.peso + "\n"
+            + "Altura: " + pessoa.altura + "\n"
+            + "Data de Nascimento: " + pessoa.dataNascimento + "\n"
+            + "IMC: " + pessoa.imc() + "\n"
+            + "Calculo IMC: " + pessoa.imcTexto()
+        );
     }
 
     public static void contaCorrente() {
@@ -85,6 +100,26 @@ public class ExercicioCinco {
     }
 
     public static void cifraCesar() {
+        char[] alfabeto = new char[]{ 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z' };
+        String mensagem = "ataque a roma";
+        int seed = 1;
+        String mensagemCript = "";
         
+        for (int contadorUm = 0; contadorUm < mensagem.length(); contadorUm++) {
+            char letra = mensagem.charAt(contadorUm);
+            if (letra != ' ') {
+                int letraIndice = 0;
+                for (int contadorDois = 0; contadorDois < alfabeto.length; contadorDois++) {
+                    if (alfabeto[contadorDois] == letra) {
+                        letraIndice = contadorDois;
+                        break;
+                    }
+                }
+                mensagemCript += alfabeto[letraIndice + seed];
+            } else {
+                mensagemCript += " ";
+            }
+        }
+        System.out.println(mensagemCript);
     }
 }
